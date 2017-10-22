9e0e0d9a71e73e6d26891f50bb5b29b55ef32166
Sergiu14 <scribansergiu@gmail.com>
2017-10-22 10:00:45 +0300

    v1.0
    multi threading
    part screen blonking

--- mainDisplayEngineParameters.py
+++ mainDisplayEngineParameters.py
@@ -1,7 +1,7 @@
 from main import *
 import threading
 from main import stopThreads
-
+from tkinter import *
 
 stopThreads = False
 changePanelVariable
@@ -13,7 +13,7 @@ def changeStopThreads(newStopThreads):
 
 def IncrementVariablePanel(newVariable):
     global changePanelVariable
-    changePanelVariable = newVariable +1
+    changePanelVariable = newVariable
 
 
 class displayEngineParameters(threading.Thread):
@@ -41,6 +41,9 @@ class displayEngineParameters(threading.Thread):
         textEngineTorque = "Torque: " + str(self.valueTest) + "N/m"
         textTurbochargerPressure = "Turbocharger pressure: " + str(self.valueTest) + "kPa"
         textFuelTemp = "Fuel Temp: " + str(self.valueTest) + "°"
+        textDateAndTime="xx:xx:xx yy/yy/yy"
+        textConsumption="Consumption: " + "l/100km"
+        textRange="Range:  km"
 
 
 
@@ -48,47 +51,65 @@ class displayEngineParameters(threading.Thread):
 
             global stopThreads
             global changePanelVariable
-            print(str(changePanelVariable))
+            #print(str(changePanelVariable))
+            time.sleep(0.05)
+
 
             if stopThreads == True:   #exit from thread
                 os._exit(1)
 
-            if changePanelVariable % 2 ==0 :
+            if changePanelVariable == 0:
+                label1 = myfont.render(str(textConsumption), 1, (255,50,0))
+                screen.blit(label1, (550, 190))
+
+                label1 = myfont.render(str(textRange), 1, (255, 50, 0))
+                screen.blit(label1, (550, 240))
+
+            if changePanelVariable == 1:
                 #first table info
-                label1 = myfont.render(str(textEngineRPM), 1, (255, 255, 0))
-                screen.blit(label1, (100, 100))
+                label1 = myfont.render(str(textEngineRPM), 1, (255,50,0))
+                screen.blit(label1, (510, 110))
 
-                label2 = myfont.render(str(textVehicleSpeed), 1, (255, 255, 0))
-                screen.blit(label2, (100, 120))
+                label2 = myfont.render(str(textVehicleSpeed), 1, (255,50,0))
+                screen.blit(label2, (510, 150))
 
-                label3 = myfont.render(str(textEngineCoolantTemp), 1, (255, 255, 0))
-                screen.blit(label3, (100, 140))
+                label3 = myfont.render(str(textEngineCoolantTemp), 1, (255,50,0))
+                screen.blit(label3, (510, 190))
 
-                label4 = myfont.render(str(textFuelTemp), 1, (255, 255, 0))
-                screen.blit(label4, (100, 160))
+                label4 = myfont.render(str(textFuelTemp), 1, (255,50,0))
+                screen.blit(label4, (510, 230))
 
-                label5 = myfont.render(str(textOilTemp), 1, (255, 255, 0))
-                screen.blit(label5, (100, 180))
+                label5 = myfont.render(str(textOilTemp), 1, (255,50,0))
+                screen.blit(label5, (510, 270))
 
             ###########################################################################################################
             # second table info
-            if changePanelVariable % 2 == 1:
-                label6 = myfont.render(str(textThrottlePos), 1, (255, 255, 0))
-                screen.blit(label6, (100, 100))
+            if changePanelVariable == 2:
+                label6 = myfont.render(str(textThrottlePos), 1, (255,50,0))
+                screen.blit(label6, (510, 110))
 
-                label7 = myfont.render(str(textIntakeAirTemp), 1, (255, 255, 0))
-                screen.blit(label7, (100, 120))
+                label7 = myfont.render(str(textIntakeAirTemp), 1, (255,50,0))
+                screen.blit(label7, (510, 150))
 
-                label8 = myfont.render(str(textIntakeManifoldAbsoluteressure), 1, (255, 255, 0))
-                screen.blit(label8, (100, 140))
+                label8 = myfont.render(str(textIntakeManifoldAbsoluteressure), 1, (255,50,0))
+                screen.blit(label8, (510, 190))
 
-                label9 = myfont.render(str(textEngineTorque), 1, (255, 255, 0))
-                screen.blit(label9, (100, 160))
+                label9 = myfont.render(str(textEngineTorque), 1, (255,50,0))
+                screen.blit(label9, (510, 230))
 
-                label10 = myfont.render(str(textTurbochargerPressure), 1, (255, 255, 0))
-                screen.blit(label10, (100, 180))
+                label10 = myfont.render(str(textTurbochargerPressure), 1, (255,50,0))
+                screen.blit(label10, (510, 270))
 
-            pygame.display.flip()
+            label11 = myfont.render(str(textDateAndTime), 1, (255,50,0))
+            screen.blit(label11, (560, 350))
+
+            pygame.draw.rect(screen, (255,50,0), (500, 100, 300, 350), 2)
+            pygame.display.update()
+
+            time.sleep(1)
+
+            screen.fill((0,0,0),(510,110,310,360))
 
 
-            clock.tick(500)
+            #pygame.display.flip()
+            clock.tick(60)
