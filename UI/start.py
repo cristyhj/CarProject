9f4e80b93da2de669c5516fce4c9137ec6d77e5e
andrei <andrei_cristian57@yahoo.com>
2017-11-21 01:12:42 +0200

    progress

--- UI/start.py
+++ UI/start.py
@@ -8,7 +8,7 @@ from kivy.uix.gridlayout import GridLayout
 from kivy.uix.screenmanager import FadeTransition
 import subprocess
 import os
-#from transdata import *
+from persistentdata import *
 
 #Builder.load_file('start.kv')
 
@@ -36,16 +36,24 @@ class BtnWidget(ButtonBehavior, Image):
         sm.current = 'menu'
         
     def cb_obd(instance):
-        #proc = subprocess.Popen('../DashBoard/main.py', shell=True)
-        #proc.send_signal(2)
-        #os.system('cd ..')
-        #os.system('cd .. && python main.py')
         fd = os.open(fifofile, os.O_WRONLY)
         os.write(fd, b'start main')
         os.close(fd)
+        exit()
+
+    def cb_navit(instance):
+        fd = os.open(fifofile, os.O_WRONLY)
+        os.write(fd, b'start navi')
+        os.close(fd)
+        exit()
         
+    def cb_options(instance):
+        x = load_variable("volume")
+        print(x)
+        save_variable("volume", x + 0.1)
         
     def cb_power_off(instance):
+        save_on_exit()
         exit()
 
 
@@ -59,6 +67,7 @@ class MyApp(App):
         
         btn_navit = BtnWidget()
         btn_navit.source = 'images/open.png'
+        btn_music_player.bind(on_press=BtnWidget.cb_navit)
         
         btn_obd = BtnWidget()
         btn_obd.source = 'images/egalizator.png'
@@ -66,6 +75,7 @@ class MyApp(App):
         
         btn4 = BtnWidget()
         btn4.source = 'images/settings.png'
+        btn4.bind(on_press=BtnWidget.cb_options)
         
         btn5 = BtnWidget()
         btn5.source = 'images/menu.png'
--- supervisor.c
+++ supervisor.c
@@ -1,29 +1,30 @@
 #include <fcntl.h>
 #include <sys/stat.h>
-#include <sys/types.h>
 #include <unistd.h>
 #include <stdio.h>
 #include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 
 #define LEN 64
 
     // start another process as a child
-void startProcess(const char *path) {
-    int childPid = fork();
+pid_t startProcess(const char *path) {
+    pid_t childPid = fork();
     if (childPid == -1) {
         printf("fork error\n");
-        return 1;
+        return -1;
     } else if (childPid == 0) {
         // here starts the UI program as a child process
         execv(path, (char*const*)NULL);
-        printf("continua aparent\n");
+        printf("continua aparent (adica e eroare - nu s-a deschis path-ul: %s!\n", path);
     }
     // here continues the parent process
-    return;
+    return childPid;
 }
 
 int main() {
-    int childPid;
+    pid_t pidUI;
     int fd;
     char *fifoFile = "/tmp/fifofile";
     char *pathToStart = "/media/andrei/Data/ProgramsData/Python/CarProject/UI/start.py";
@@ -33,18 +34,23 @@ int main() {
     
     mkfifo(fifoFile, 0666);
     
-    startProcess(pathToStart);
+    // prima data porneste programul UI
+    pidUI = startProcess(pathToStart);
     
     // here continues the parent process
     fd = open(fifoFile, O_RDONLY);
+    
+    // asteapta comanda de la parinte, sau alte procese
     while (1) {
         read(fd, buff, LEN);
         buff[10] = '\0';
         if (strcmp(buff, "start main") == 0) {
+            waitpid(pidUI, 0, 0);
             startProcess(pathToMain);
             break;
         }
         if (strcmp(buff, "start navi") == 0) {
+            waitpid(pidUI, 0, 0);
             startProcess(pathToNavit);
             break;
         }
